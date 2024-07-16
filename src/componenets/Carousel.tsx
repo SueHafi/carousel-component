3fb31ed5958c1147533624df098edbefd8fa7d91
sue rose <suehaizoglu@gmail.com>
2024-07-16 13:17:12 +0100

    text section implemented

--- src/componenets/Carousel.tsx
+++ src/componenets/Carousel.tsx
@@ -1,5 +1,19 @@
-import './Carousel.css'
+import "./Carousel.css";
 
 export default function Carousel() {
-  return <div></div>
+  return (
+    <div className="carousel-container">
+      <img></img>
+      <div className="text-container">
+        <p className="text paragraph">
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
+          dolores, laborum repellat eos amet a inventore perspiciatis eveniet
+          adipisci, obcaecati nesciunt aspernatur commodi? Adipisci, fugiat
+          veniam. Amet ad laudantium minima?"
+        </p>
+        <p className="text author">Susana Jeffory</p>
+        <p className="text company">Senior Developer at Global Energy LTD</p>
+      </div>
+    </div>
+  );
 }
