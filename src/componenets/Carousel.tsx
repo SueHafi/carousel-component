57541252977cd442dfdaadb69a159982f69180eb
sue rose <suehaizoglu@gmail.com>
2024-07-18 12:06:36 +0100

    updated carousel information to pull from state

--- src/componenets/Carousel.tsx
+++ src/componenets/Carousel.tsx
@@ -1,27 +1,63 @@
 import { useState } from "react";
 import "./Carousel.css";
 
+type carouselContentTypes = [
+  {
+    img: string;
+    imgDescription: string;
+    text: string;
+    author: string;
+    companyRole: string;
+  }
+];
+
 export default function Carousel() {
-  const [authorImage, setAuthorImage] = useState("/wendy-fox.jpg");
-  const [imgDescription, setImgDescription] = useState("blonde lady smiling");
-  const authorArray = [
-    { author: "/wendy-fox.jpg", description: "blonde lady smiling" },
-    { author: "/susana-jeffory.jpg", description: "brown haired lady smiling" },
+  const [carouselContent, setCarouselContent] = useState<carouselContentTypes>([
     {
-      author: "/robert-dinero.jpg",
-      description: "brown haired young man smiling",
+      img: "/wendy-fox.jpg",
+      imgDescription: "blonde lady smiling",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, dolores, laborum repellat eos amet a inventore perspiciatis eveniet adipisci, obcaecati nesciunt aspernatur commodi? Adipisci, fugiat veniam. Amet ad laudantium minima?",
+      author: "Wendy Fox",
+      companyRole: "Senior Developer at Global Energy LTD",
+    },
+  ]);
+
+  const contentArray = [
+    {
+      img: "/wendy-fox.jpg",
+      imgDescription: "blonde lady smiling",
+      text: "blah",
+      author: "Wendy Fox",
+      companyRole: "Senior Developer at Global Energy LTD",
     },
     {
-      author: "/jack-daniels.jpg",
-      description: "brown haired man with sun glasses smiling",
+      img: "/susana-jeffory.jpg",
+      imgDescription: "brown haired lady smiling",
+      text: "blah",
+      author: "Susana Jeffory",
+      companyRole: "Nurse at National Health wellfare",
+    },
+    {
+      img: "/robert-dinero.jpg",
+      imgDescription: "brown haired young man smiling",
+      text: "blah",
+      author: "Robert Dinero",
+      companyRole: "Executive Producer at Musical Acadmey-Top Arts",
+    },
+    {
+      img: "/jack-daniels.jpg",
+      imgDescription: "brown haired man with sun glasses smiling",
+      text: "blah",
+      author: "Jack Daniels",
+      companyRole: "Headteacher at Moonlight Peaks Academy",
     },
   ];
 
   function handleOnClick(buttonClick: string): void {
-    if(buttonClick === 'rightButton') {
-      console.log('right button clicked');
+    if (buttonClick === "rightButton") {
+      console.log("right button clicked");
     } else {
-      console.log('left button clicked');
+      console.log("left button clicked");
     }
   }
 
@@ -29,30 +65,27 @@ export default function Carousel() {
     <div className="carousel-container">
       <button
         className="button left-button"
-        onClick={() => handleOnClick('leftButton')}
+        onClick={() => handleOnClick("leftButton")}
       >
         &#10092;
       </button>
       <div className="content-container">
         <img
           className="author-img"
-          src={authorImage}
-          alt={imgDescription}
+          src={carouselContent[0].img}
+          alt={carouselContent[0].imgDescription}
         ></img>
         <blockquote className="text-container">
           <p className="text paragraph">
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
-            dolores, laborum repellat eos amet a inventore perspiciatis eveniet
-            adipisci, obcaecati nesciunt aspernatur commodi? Adipisci, fugiat
-            veniam. Amet ad laudantium minima?"
+      {carouselContent[0].text}
           </p>
-          <p className="text author">Susana Jeffory</p>
-          <p className="text company">Senior Developer at Global Energy LTD</p>
+          <p className="text author">{carouselContent[0].author}</p>
+          <p className="text company">{carouselContent[0].companyRole}</p>
         </blockquote>
       </div>
       <button
         className="button right-button"
-        onClick={() => handleOnClick('rightButton')}
+        onClick={() => handleOnClick("rightButton")}
       >
         &#10093;
       </button>
